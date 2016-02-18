393c607bed7759e384ff7ec96dc4e2f8142bda01
NJNugget <jiangziyunhao@sina.com>
2016-02-18 17:53:48 +0800

    关于页面添加TableView

--- AboutViewController.swift
+++ AboutViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet var profileTableView: UITableView!
     @IBOutlet var profileImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,10 +18,26 @@ class AboutViewController: UIViewController {
         profileImage.backgroundColor = UIColor.blackColor()
         profileImage.layer.cornerRadius = 60.0
         profileImage.layer.masksToBounds = true
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+        profileTableView.tableFooterView = UIView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AboutCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "1233"
+        return cell
+    }
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
 }
